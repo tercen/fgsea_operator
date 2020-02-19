f61e9e802be76470087d5ea01c390832bf006beb
infoTercengit config user.email info@tercen.com <info@tercn.com>
2020-02-19 06:38:34 +0000

    fixed code and updated renv

--- main.R
+++ main.R
@@ -1,51 +1,36 @@
 library(tercen)
 library(dplyr)
 library(fgsea)
+library(tidyverse)
+
 
 ctx = tercenCtx()
 
-gene_factor_name_char <- ctx$rnames[[1]]
-genes <- ctx$rselect(ctx$rnames)
-genes_list <- as.character(unlist(genes))
+pathway_factor_name_char <- ctx$rnames[[1]]
+pathway_df <- ctx$rselect(ctx$rnames) %>% mutate(.ri = 0:(nrow(.)-1))
+gene_factor_name <- sym(ctx$labels[[1]])
+gene_factor_char <- ctx$labels[[1]]
 
-pathways_df <- ctx$cselect(ctx$cnames)
-pathways_df <- bind_cols(pathways_df, col_idx = (0:(nrow(pathways_df)-1)))
-names(pathways_df) <- c("pathway", ".ci")
-
-in_matrix <- as.tibble(ctx$as.matrix())
-values <- (in_matrix != 0)
-values <- unnest(as.tibble(values))
-
-
-# values <- bind_cols(genes, in_table)
-in_table <- ctx$select()
+in_table <- ctx$select(list(".ci", ".ri", ".y", ctx$labels[[1]]))
 
 gene_df <- in_table %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ri) %>%
-  summarise(rank = mean(.y)) %>%
-  bind_cols(genes) 
+  select(.y, !!gene_factor_name) %>%
+  group_by(!!gene_factor_name) %>%
+  summarise(value = mean(.y)) %>%
+  column_to_rownames(ctx$labels[[1]])
 
-rank_list <- gene_df$rank
-names(rank_list) <- gene_df[[gene_factor_name_char]]
+rank_list <- gene_df$value
+names(rank_list) <- rownames(gene_df)
 
-getGeneId <- function(alist) genes_list[alist]
+getGeneId <- function(df,...){as.character(unlist((df[[gene_factor_char]])))}
+pathway_list <- in_table %>% group_by(.ri) %>% group_split %>% map(getGeneId)
+pathway_names <- pathway_df[[pathway_factor_name_char <- ctx$rnames[[1]]]]
+names(pathway_list) <- pathway_names
 
-pathway_list <- lapply(values, getGeneId)
+fgseaRes <- fgsea(pathway_list, rank_list, maxSize=500, nperm=10000)
 
-
-names(pathway_list) <- unlist(pathways_df$pathway)
-
-fgseaRes <- fgsea(pathway_list, rank_list, maxSize=500)
-
-
-# fgseaRes <- fgsea(examplePathways, exampleRanks, maxSize=500)
-
-
-result <- fgseaRes %>% select(pathway, NES) %>% merge(pathways_df, by = c("pathway"))
-
+result <- fgseaRes %>% select(pathway, NES, padj) %>% left_join(pathway_df, by = c("pathway"= pathway_factor_name_char))
 
 result %>%
   ctx$addNamespace() %>%
   ctx$save()
-
